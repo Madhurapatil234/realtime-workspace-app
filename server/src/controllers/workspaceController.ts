bec6aebc362ad3567e662b6b44707bb9d2f2dd3a
Madhura Patil <madhurapatil822@gmail.com>
2026-06-14 23:25:59 +0530

    Week 2 progress: Workspace APIs and JWT middleware

--- server/src/controllers/workspaceController.ts
+++ server/src/controllers/workspaceController.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from "express";
 import Workspace from "../models/Workspace";
 
+// Create Workspace
 export const createWorkspace = async (
   req: Request,
   res: Response
 ) => {
   try {
-    console.log(req.body);
-
-const { name, description, owner } = req.body;
+    const { name, description, owner } = req.body;
 
     const workspace = await Workspace.create({
       name,
@@ -26,6 +25,27 @@ const { name, description, owner } = req.body;
 
     res.status(500).json({
       message: "Workspace Creation Failed",
+    });
+  }
+};
+
+// Get All Workspaces
+export const getAllWorkspaces = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const workspaces = await Workspace.find();
+
+    res.status(200).json({
+      count: workspaces.length,
+      workspaces,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Failed to fetch workspaces",
     });
   }
 };
--- server/src/routes/workspaceRoutes.ts
+++ server/src/routes/workspaceRoutes.ts
@@ -1,9 +1,17 @@
 import express from "express";
-import { createWorkspace } from "../controllers/workspaceController";
+import {
+  createWorkspace,
+  getAllWorkspaces,
+} from "../controllers/workspaceController";
+
 import { protect } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
+// Create Workspace
 router.post("/create", protect, createWorkspace);
+
+// Get All Workspaces
+router.get("/", protect, getAllWorkspaces);
 
 export default router;
